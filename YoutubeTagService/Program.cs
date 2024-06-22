291d52ad6beab29e671749a9b8df8690d31c160d
jlardinois <jlardinois@ilionx.com>
2024-06-22 17:23:47 +0200

    Added Dockerfile, taskdef, and appspec.yml

--- YoutubeTagService/Program.cs
+++ YoutubeTagService/Program.cs
@@ -1,3 +1,6 @@
+using YoutubeTagService.BLL;
+using YoutubeTagService.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IYoutubeTagRepository, YoutubeTagRepository>();
 
 var app = builder.Build();
 
